2d9fc05e2fdd09becfc3c2757dc462f100293b57
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2025-05-12 15:40:29 +0200

    Commit 1: Setup basic authentication

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node'
+import clerk from '@clerk/astro'
+// astro.config.mjs
+export default defineConfig({
+  integrations: [clerk()],
+  adapter: node({ mode: 'standalone' }),
+  output: 'server',
+})
 
-// https://astro.build/config
-export default defineConfig({});
