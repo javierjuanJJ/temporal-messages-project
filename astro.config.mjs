f1f1a11cfda915c0c9c3833f9f912e94f7f5fe6e
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2025-05-14 16:20:29 +0200

    Commit 5: Create mis-messages.astro and install Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node'
 import clerk from '@clerk/astro'
+import tailwindcss from "@tailwindcss/vite";
 // astro.config.mjs
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
   integrations: [clerk()],
   adapter: node({ mode: 'standalone' }),
   output: 'server',
